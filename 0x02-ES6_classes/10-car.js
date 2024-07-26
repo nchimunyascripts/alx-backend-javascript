1f14d309e89851719259c75fae9809600679e084
nchimunya-joseph <nchimunya.backlogcode@gmail.com>
2024-07-26 03:52:30 +0200

    refactor code

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -5,31 +5,11 @@ export default class Car {
     this._color = color;
   }
 
-  get brand() {
-    return this._brand;
-  }
-
-  set brand(brand) {
-    this._brand = brand;
-  }
-
-  get motor() {
-    return this._motor;
-  }
-
-  set motor(motor) {
-    this._motor = motor;
-  }
-
-  get color() {
-    return this._color;
-  }
-
-  set color(color) {
-    this._color = color;
+  static get [Symbol.species]() {
+    return this;
   }
 
   cloneCar() {
-    return new Car(this._brand, this._motor, this._color);
+    return new this.constructor[Symbol.species]();
   }
 }
